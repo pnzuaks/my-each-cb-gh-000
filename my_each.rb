385582f5b62b39a93101aa9b63a5b33163f09f92
Patricia Nykaza <pnykaza@calstatela.edu>
2018-04-12 21:47:40 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -6,27 +6,12 @@ def my_each(array)
   i = i + 1
 end
 
-  puts i 
+  print i 
   array
 end
 
 
 
 
-
-def hello_t(array)
-  if block_given?
-  i = 0 
-  
-  while i < array.length 
-  yield(array[i])
-  i = i + 1
-  end
-  
-  array
-else
-    puts "Hey! No block was given!"
-end
-end
 
 
